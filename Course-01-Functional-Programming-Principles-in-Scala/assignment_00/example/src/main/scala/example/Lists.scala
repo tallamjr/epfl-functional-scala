1783bd7d512407b7fc73a927033e5807302b16c6
Tarek Allam <t.allam.jr@gmail.com>
2020-01-06 21:30:39 +0000

    Implemented sum() and max() functions
    Test code passes
    	modified:   src/main/scala/example/Lists.scala

--- Course-01-Functional-Programming-Principles-in-Scala/assignment_00/example/src/main/scala/example/Lists.scala
+++ Course-01-Functional-Programming-Principles-in-Scala/assignment_00/example/src/main/scala/example/Lists.scala
@@ -22,7 +22,10 @@ object Lists extends App {
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-  def sum(xs: List[Int]): Int = ???
+  def sum(xs: List[Int]): Int =
+
+    if (xs.isEmpty) 0
+    else xs.head + sum(xs.tail)
 
   /**
    * This method returns the largest element in a list of integers. If the
@@ -37,5 +40,16 @@ object Lists extends App {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-  def max(xs: List[Int]): Int = ???
+  def max(xs: List[Int]): Int =
+
+    if (xs.isEmpty) throw new NoSuchElementException()
+    else _max(xs)
+
+  def _max(xs: List[Int]): Int =
+
+    if (xs.tail.isEmpty) xs.head
+    else {
+      val m = _max(xs.tail)
+      if (xs.head > m) xs.head else m
+    }
 }
