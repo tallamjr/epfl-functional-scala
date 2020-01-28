f54e657cd14e3afd80bf7dc882b3ded0b6b8ad53
Tarek Allam <t.allam.jr@gmail.com>
2020-01-28 22:46:04 +0000

    Passing tests for assignment 1
    	modified:   src/main/scala/recfun/RecFun.scala

--- Course-01-Functional-Programming-Principles-in-Scala/assignment-01/recfun/src/main/scala/recfun/RecFun.scala
+++ Course-01-Functional-Programming-Principles-in-Scala/assignment-01/recfun/src/main/scala/recfun/RecFun.scala
@@ -14,15 +14,48 @@ object RecFun extends RecFunInterface {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int =
+    if (c == 0 || c == r)
+      1
+    else
+      pascal(c - 1, r - 1) + pascal(c, r - 1)
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    if (chars.isEmpty)
+      true
+    else if (chars.head == '(')
+      check_inner(chars.tail, 1)
+    else if (chars.head == ')')
+      false
+    else balance(chars.tail)
+  }
+
+  def check_inner(chars: List[Char], counter: Int): Boolean = {
+    if (chars.isEmpty && counter != 0)
+      false
+    else if (chars.head ==')' && counter < 1)
+      false
+    else if (chars.isEmpty && counter == 0)
+      true
+    else if (!chars.contains('(') || !chars.contains(')') && counter == 0)
+      true
+    else if (chars.head == ')')
+      check_inner(chars.tail, counter - 1)
+    else if (chars.head == '(')
+      check_inner(chars.tail, counter + 1)
+    else check_inner(chars.tail, counter)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int =
+    if (money < 0 || coins.isEmpty)
+      0
+    else if (money == 0)
+      1
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
 }
